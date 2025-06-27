1e05fc2aca1ee36edede66c0447a99bf6271dd77
M.REHAN <rehansiddique962@gmail.com>
2025-06-27 21:18:21 +0500

    HICommit3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Otp from "./pages/otp"
-import Home from "./pages/home"
 import Login from "./pages/login"
 import Signup from "./pages/signup"
 import Forget from "./pages/forget"
@@ -17,8 +16,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login/>}/>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/" element={<Signup/>}/>
       <Route path="/forget" element={<Forget/>}/>
       <Route path="/otp" element={<Otp/>}/>
       <Route path="/Confirmpassword" element={<Confirm/>}/>
